adad460eb991b2e125d29d609990e593ffa277d7
Paul Water <p.r.water@setbased.nl>
2019-06-13 17:43:48 +0200

    Updated development status.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license='MIT',
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         'Intended Audience :: System Administrators',
         'Topic :: System :: Archiving :: Backup',
